36d183a11a6bd6833f29c76be543e7b13c5413bd
Sergey-15-r <sergey.alikov.98@mail.ru>
2020-03-14 12:59:17 +0300

    седьмое задание

--- js/form.js
+++ js/form.js
@@ -5,8 +5,12 @@
     OK: 200,
     ERROR: 400
   };
+  var MAIN_PIN_LOCATION = 'left: 570px; top: 375px;';
+  var mapElement = document.querySelector('.map');
   var pinMain = document.querySelector('.map__pin--main');
   var adForm = document.querySelector('.ad-form');
+  var filterForm = document.querySelector('.map__filters');
+  var formReset = document.querySelector('.ad-form__reset');
   var adFormElements = adForm.querySelectorAll('fieldset');
   var roomNumber = document.querySelector('#room_number');
   var capacity = document.querySelector('#capacity');
@@ -24,10 +28,13 @@
     .content
     .querySelector('.error');
 
+  var setEnabledForm = function (flag) {
+    for (var i = 0; i < adFormElements.length; i++) {
+      adFormElements[i].disabled = flag;
+    }
+  };
 
-  for (var i = 0; i < adFormElements.length; i++) {
-    adFormElements[i].disabled = true;
-  }
+  setEnabledForm(true);
 
   var setFormAddress = function (left, top) {
     address.value = left + ', ' + top;
@@ -88,6 +95,24 @@
     timein.value = timeout.value;
   });
 
+  var resetScreen = function () {
+    adForm.reset();
+    filterForm.reset();
+    setEnabledForm(true);
+    mapElement.classList.add('map--faded');
+    adForm.classList.add('ad-form--disabled');
+    // вернуть координаты main pin
+    pinMain.style = MAIN_PIN_LOCATION;
+    setFormAddress(pinMain.offsetLeft, pinMain.offsetTop);
+
+    // очистить карту от всех обьявлений
+    document.querySelectorAll('.map__pin:not(.map__pin--main)').forEach(function (el) {
+      el.remove();
+    });
+
+  };
+
+  formReset.addEventListener('click', resetScreen);
 
   adForm.addEventListener('submit', function (evt) {
     window.upload(new FormData(adForm), function (xhr) {
@@ -108,6 +133,7 @@
 
     if (successDiv !== null) {
       successDiv.remove();
+      resetScreen();
     }
   });
 
@@ -115,13 +141,15 @@
     var successDiv = document.querySelector('.success');
     if (evt.key === 'Escape' && successDiv !== null) {
       successDiv.remove();
+      resetScreen();
     }
   };
 
   document.addEventListener('keydown', onPopupEscPress);
 
   window.form = {
-    setFormAddress: setFormAddress
+    setFormAddress: setFormAddress,
+    setEnabledForm: setEnabledForm
   };
 
 })();
--- js/pin.js
+++ js/pin.js
@@ -1,20 +1,22 @@
 'use strict';
 (function () {
 
+  var PIN_WIDTH = 50;
+  var PIN_HEIGHT = 70;
 
   var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
-  var renderListing = function (listing, index) {
+  var renderListing = function (listing) {
     var listingElement = pinTemplate.cloneNode(true);
 
     listingElement.querySelector('img').src = listing.author.avatar;
     listingElement.querySelector('img').alt = listing.offer.title;
-    listingElement.style.left = listing.location.x + 'px';
-    listingElement.style.top = listing.location.y + 'px';
+    listingElement.style.left = (listing.location.x - PIN_WIDTH / 2) + 'px';
+    listingElement.style.top = (listing.location.y - PIN_HEIGHT) + 'px';
 
-    listingElement.dataset.indexNumber = index;
+    listingElement.dataset.indexNumber = listing.id;
     listingElement.addEventListener('click', window.card.displayPopup);
 
     return listingElement;
